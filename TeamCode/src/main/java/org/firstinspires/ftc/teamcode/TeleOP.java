cdfa32ab2070c8f0f3bf90d4ed00d5ca6a83474c
tortillero32 <ernestormzg09@gmail.com>
2026-06-10 19:56:31 -0600

    primer reto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP.java
@@ -0,0 +1,125 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import android.os.Handler;
+import android.os.Looper;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Nigga", group="Linear Opmode")
+
+public class TeleOP extends LinearOpMode {
+
+    DcMotor driveRight = null;
+    DcMotor driveLeft = null;
+    DcMotor intake = null;
+    private double driveVelocityCap = 1;
+
+
+
+    private int tpos = 0;
+
+    @Override
+    public void runOpMode() {
+
+
+        //Inicialización de motores
+        driveLeft = hardwareMap.get(DcMotor.class, "leftDrive");
+        driveRight = hardwareMap.get(DcMotor.class, "rightDrive");
+        intake = hardwareMap.get(DcMotor.class, "intake");
+
+
+        driveLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        driveLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        driveLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        driveRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        driveRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        driveRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        intake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        intake.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Invertimos un lado para que todos avancen hacia adelante
+        driveLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        driveRight.setDirection(DcMotorSimple.Direction.FORWARD);
+
+
+        // Espera a que empiece el match
+        waitForStart();
+
+
+
+        // Loop principal de teleop
+        while (opModeIsActive()) {
+
+
+            double fwd = gamepad2.left_stick_y;
+            double turn = gamepad2.right_stick_x;
+
+
+            double denominator = Math.max(Math.abs(fwd) + Math.abs(turn) , 1);
+            double leftPwr = ((fwd + turn) / denominator) * driveVelocityCap;
+            double rightPwr = ((fwd - turn) / denominator) * driveVelocityCap;
+
+            // Normalizamos si algún valor > 1
+
+
+            driveRight.setPower(rightPwr);
+            driveLeft.setPower(leftPwr);
+
+            //triggers
+            if (gamepad2.right_trigger > 0.5) {
+                intake.setPower(-0.9);
+
+            } else if (gamepad2.left_trigger > 0.5) {
+                intake.setPower(0.5);
+
+            } else {
+                intake.setPower(0);
+            }
+
+// SOY GAY = True
+// atte.Hachita
+
+                    //Telemetría para ver valores en Driver Station
+            telemetry.addData("Right PWR", rightPwr);
+            telemetry.addData("Left PWR", leftPwr);
+            telemetry.addData("Intake", intake.getPower());
+
+            telemetry.update();
+
+                }
+            }
+        }
+
